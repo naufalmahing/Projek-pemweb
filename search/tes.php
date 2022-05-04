8aad617b4352ea2599bf4a1b31cc6aa4bfd1b0fd
Naufal Fathirachman Mahing <71597476+naufalmahing@users.noreply.github.com>
2022-05-04 12:36:40 +0700

    Create tes.php

---
+++ search/tes.php
@@ -0,0 +1,5 @@
+<body>
+    <h1>Tes search php bang</h1>
+    <?php include 'search.php'; // baikin position di html bang biar ga ngaruh ke elemen lain?>
+    <p>kalo ada paragraf di bawah gimana?</p>
+</body>
