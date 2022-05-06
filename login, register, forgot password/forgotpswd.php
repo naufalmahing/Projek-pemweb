2919384fad37752e42c60225b4e88ed76fb2bc57
Naufal Fathirachman Mahing <71597476+naufalmahing@users.noreply.github.com>
2022-05-06 08:16:38 +0700

    upload vendor folder dan sesuaikan path

--- login, register, forgot password/forgotpswd.php
+++ login, register, forgot password/forgotpswd.php
@@ -4,9 +4,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require __DIR__.'/../vendor/autoload.php';
 require 'conndbs.php';
 
 
