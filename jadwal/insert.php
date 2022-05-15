215fbddbece0eb225b3017c7de93d5d46b57a627
Naufal Fathirachman Mahing <71597476+naufalmahing@users.noreply.github.com>
2022-05-16 06:58:31 +0700

    hapus yang tidak penting

--- jadwal/insert.php
+++ jadwal/insert.php
@@ -1,15 +1,12 @@
 <?php
 include '../reader/koneksi.php';
 
-// $connect = new PDO('mysql:host=localhost;dbname=test', 'root', 'pastipasti');
-
 $email = 'naufalmahing@gmail.com';
 if (isset($_COOKIE['email'])) { 
     $email = $_COOKIE['email'];
 }
 
 if(isset($_POST['title'])) {
-    $query = "INSERT INTO events (title, start_event, end_event, email) VALUES (:title, :start_event, :end_event, :email)";
     $query = "INSERT INTO events (title, start_event, end_event, email) VALUES (?, ?, ?, ?)";
     $statement = $koneksi ->prepare($query);
     $statement->bind_param('ssss', $_POST['title'],
